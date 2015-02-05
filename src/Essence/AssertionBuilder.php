b70a46c89943c75285ae26d656e518c168be2ad3
bound1ess <bound1ess.dev@gmail.com>
2015-02-05 20:00:25 +0600

    Write method stubs.

--- src/Essence/AssertionBuilder.php
+++ src/Essence/AssertionBuilder.php
@@ -4,4 +4,24 @@ class AssertionBuilder
 {
 
     use \PhpPackages\Fluent\FluentTrait;
+
+    /**
+     * Performs the validation.
+     *
+     * @return boolean
+     */
+    public function validate()
+    {
+        return false; // or true
+    }
+
+    /**
+     * Returns the error message (null if there is none).
+     *
+     * @return string|null
+     */
+    public function getMessage()
+    {
+        return "something or null";
+    }
 }
