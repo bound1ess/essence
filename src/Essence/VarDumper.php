6f3468d0d003b8449e05ca9fd08e54cc77e29427
bound1ess <bound1ess.dev@gmail.com>
2015-02-11 18:21:24 +0600

    Add support for strings.

--- src/Essence/VarDumper.php
+++ src/Essence/VarDumper.php
@@ -5,4 +5,22 @@
  */
 class VarDumper
 {
+
+    /**
+     * Dumps a value.
+     *
+     * @param mixed $value
+     * @return string
+     */
+    public function dump($value)
+    {
+        $value = str_replace(PHP_EOL, "\\n", $value);
+
+        // @suggestion multibyte support?
+        if (strlen($value) > 30) {
+            $value = substr($value, 0, 30)."...";
+        }
+
+        return $value;
+    }
 }
--- tests/Essence/VarDumperTest.php
+++ tests/Essence/VarDumperTest.php
@@ -4,4 +4,23 @@ class VarDumperTest extends \TestCase
 {
 
     protected $subject = "Essence\VarDumper";
+
+    /**
+     * @test
+     */
+    public function it_prints_a_string()
+    {
+        $this->assertEquals($this->subject->dump("foobar"), "foobar");
+        $this->assertEquals($this->subject->dump("foo".PHP_EOL."bar"), "foo\\nbar");
+
+        $this->assertEquals(
+            $this->subject->dump(str_repeat("a", 100)),
+            str_repeat("a", 30)."..."
+        );
+
+        $this->assertEquals(
+            $this->subject->dump("foobar".PHP_EOL.str_repeat("a", 30)),
+            "foobar\\n".str_repeat("a", 22)."..."
+        );
+    }
 }
