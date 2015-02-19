60dee3e3d19dfd56c66d68597864bcf6e9a7f69f
bound1ess <bound1ess.dev@gmail.com>
2015-02-19 19:00:20 +0600

    Something like that.

--- src/Essence/Extensions/PhpunitExtension.php
+++ src/Essence/Extensions/PhpunitExtension.php
@@ -11,6 +11,16 @@ class PhpunitExtension extends \PHPUnit_Framework_TestCase
      */
     public function tearDown()
     {
-        // @todo
+        // If Mockery is utilised.
+        if (class_exists("Mockery")) {
+            \Mockery::close();
+        }
+
+        $assertions = essence()->validateAll();
+
+        for ($index = 0; $index < $assertions; $index += 1) {
+            // The fastest one (I believe).
+            $this->assertTrue(true);
+        }
     }
 }
