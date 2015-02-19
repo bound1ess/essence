9912d70406137e56a7f65aadb2fd3d52549e06a7
bound1ess <bound1ess.dev@gmail.com>
2015-02-19 18:41:30 +0600

    Add PhpunitExtension class.

---
+++ src/Essence/Extensions/PhpunitExtension.php
@@ -0,0 +1,16 @@
+<?php namespace Essence\Extensions;
+
+/**
+ * This class helps you integrate Essence into PHPUnit.
+ */
+class PhpunitExtension extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function tearDown()
+    {
+        // @todo
+    }
+}
