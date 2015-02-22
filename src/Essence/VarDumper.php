616d598de89736972842e414d5a5bed47cdb1fb5
bound1ess <bound1ess.dev@gmail.com>
2015-02-22 23:01:02 +0600

    Connect Essence to Dumpy via VarDumper.

--- src/Essence/VarDumper.php
+++ src/Essence/VarDumper.php
@@ -1,46 +1,9 @@
 <?php namespace Essence;
 
 /**
- * Provides string representations for any of PHP data types.
+ * Connects Dumpy (@php-packages/dumpy) to Essence.
+ * @codeCoverageIgnore
  */
-class VarDumper
+class VarDumper extends \PhpPackages\Dumpy\Dumpy
 {
-
-    /**
-     * Dumps a value.
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function dump($value)
-    {
-        switch (gettype($value)) {
-
-            case "string":
-                $value = str_replace(PHP_EOL, "\\n", $value);
-
-                // @suggestion multibyte support?
-                if (strlen($value) > 30) {
-                    $value = substr($value, 0, 30)."...";
-                }
-
-                return "'{$value}'";
-
-            case "integer": return (string)$value;
-
-            case "double": return (string)$value;
-
-            case "boolean": return $value ? "true" : "false";
-
-            case "NULL": return "null";
-
-            case "object":
-                return sprintf("%s(#%s)", get_class($value), spl_object_hash($value));
-
-            case "array": return sprintf("array[%s]", count($value));
-
-            // @codeCoverageIgnoreStart
-        }
-    }
-    // @codeCoverageIgnoreEnd
 }
